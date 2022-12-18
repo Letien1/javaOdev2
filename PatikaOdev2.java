09d50aa00092c77fabfddbe5c6fb93c4dae72bde
Letien1 <118589857+Letien1@users.noreply.github.com>
2022-12-18 09:49:20 +0300

    Add files via upload

---
+++ PatikaOdev2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PatikaOdev2 {
+    public static void main(String[] args) {
+        double tutar, kdvOrani, kdvTutari,  kdv1 = 0.18, kdv2 = 0.08, toplamTutar;
+        
+        Scanner oku = new Scanner(System.in);
+        System.out.println("Tutar Giriniz: ");
+        tutar = oku.nextDouble();
+
+        kdvOrani = tutar < 1000 ? kdv1 :  kdv2;
+        kdvTutari = tutar < 1000 ? tutar * kdv1 : tutar * kdv2;
+        toplamTutar = tutar + kdvTutari;
+
+        System.out.println("KDV'siz Tutar: " + tutar);
+        System.out.println("KDV Oranı: " + kdvOrani);
+        System.out.println("KDV Tutarı: " + kdvTutari);
+        System.out.println("Toplam Tutar: " + toplamTutar);
+    }
+}
